b9196c16fac00ff0e8517039a18a868d35cbfa3c
Nazmul1582 <nazmulhasan1582@gmail.com>
2022-12-27 11:01:59 +0600

    advance theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,6 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.html"],
   theme: {
-    screens: {
-      sm: "540px",
-      lg: "992px",
-      "4xl": "1900px",
-    },
     colors: {
       blue: "#1fb6ff",
       pink: "#ff49db",
@@ -17,6 +12,11 @@ module.exports = {
       sans: ["ubuntu", ...defaultTheme.fontFamily.sans],
     },
     extend: {
+      screens: {
+        sm: "540px",
+        lg: "992px",
+        "4xl": "1900px",
+      },
       spacing: {
         2: "20px",
       },
