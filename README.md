d826a27c28063efff3f6d0e04f435c0066c0f089
Nazmul1582 <nazmulhasan1582@gmail.com>
2022-12-27 11:45:18 +0600

    Extend Theme - Advanced Concepts - Lesson16

--- README.md
+++ README.md
@@ -1,33 +1,24 @@
 <br />
  <p align="center">
-    <h1 align="center">  Just in Time - Advanced Concepts - Lesson14</h1>
+    <h1 align="center">  Extend Theme - Advanced Concepts - Lesson16</h1>
 </p>
 
 <!-- TABLE OF CONTENTS -->
 
 ## Table of Contents
 
-- [Just-in-Time](#just-in-time)
+- [Output](#output))
 
 <br>
 
-# Just-in-Time
+# Output
 
-আগের classic engine টা just-in-time দ্বারা replace হয়ে গেছে।
-রান টাইমে(সময় মত) build করতে পারে। lightening fast build দিবে সে। আমরা এখন চোখের নিমিষে আউটপুট দেখতে পাবো।
+- Before fontFamily extend
 
-- lightening fast build
-- Stackable variants
-- Abitary value support
+![Before fontFamily extend](./images/before-font-family-extend.png)
 
-আগে বিশাল বড় file size তৈরি হতো। যেটাকে file size ballooning problem বলা হতো। এখন এই problem টা থাকবে না।
+- After fontFamily extend
 
-- Before JIT
-
-![Before JIT](./images/before-jit.png)
-
-- After JIT
-
-  ![After JIT](./images/after-jit.png)
+![After fontFamily extend](./images/after-font-family-extend.png)
 
 <br>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.html"],
   theme: {
-    /*
-    // this colors object(outside the extend object) will replace the tailwind's colors object. So, tailwind's other colors classes will not work now.
-    colors:{
-      gray:{
-        300: "#ddd"
-      }
-    },
-  */
-
-    /*
-    // this colors object(inside the theme.extend object) will replace only tailwind's colors.gray.300 property.
     extend: {
-      colors:{
-        gray:{
-          300: "#ddd"
-        }
-      }
+      fontFamily: {
+        sans: ["ubuntu", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
-  */
-
-    /* if I use this colors object, I won't use tailwind's other colors
-  colors: {
-    'blue': '#1fb6ff',
-    'purple': '#7e5bef',
-    'pink': '#ff49db',
-    'orange': '#ff7849',
-    'green': '#13ce66',
-    'yellow': '#ffc82c',
-    'gray-dark': '#273444',
-    'gray': '#8492a6',
-    'gray-light': '#d3dce6',
-  },
-  */
-    extend: {},
-  },
-  plugins: [
-    // here we write down third party or our own plugins name
-  ],
+  plugins: [],
 };
-
-// we can customizing anyting in our css file (./src/styles.css). but if we customize something here, we will get some extra facilities. (like: we will be suggeted our custom class by Tailwind InteliSence plugin etc.)
